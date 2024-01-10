c75ce7eba39d6c1fd9803ca2facdcd13d92188e4
Darío Clavijo <clavijodario@gmail.com>
2024-01-10 13:11:57 -0300

    Update pickling.py

--- lib/pickling.py
+++ lib/pickling.py
@@ -6,13 +6,13 @@ import sys
 
 # Pickle a file and then compress it into a file with extension 
 def compress_pickle(filename, data):
-    sys.stderr.write(f"Saving pickle {filename}...")
+    sys.stderr.write(f"Saving pickle {filename}...\n")
     with bz2.BZ2File(filename, 'w') as f: 
         cPickle.dump(data, f)
 
 # Load any compressed pickle file
 def decompress_pickle(filename):
-    sys.stderr.write(f"Loading pickle {filename}...")
+    sys.stderr.write(f"Loading pickle {filename}...\n")
     data = bz2.BZ2File(filename, 'rb')
     data = cPickle.load(data)
     return data
